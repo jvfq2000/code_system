9f84af6e8aa06550822a5bb20c8103caeb735370
xXLuizXx <luizfernandofonseca123@gmail.com>
2020-03-10 14:09:59 -0300

    * Motivo que deu origem a mudança;  - Alteração no arquivo cadastro.php.  - Modelação do arquivo login.php.  - Inserção da imagem top-complementary_hours.png.
    * Nome do objeto alterado - Descrição dos itens alterados (incluídos, excluídos, modificados);
     - Implementação/complementary-hours/application/view/cadastro.php
     - Implementação/complementary-hours/application/view/login.php
     - Implementação/complementary-hours/assets/img/top-complementari_hours.png.
    
    * Sinalização de outros objetos que podem ser impactados com a alteração;
     - Nada
    
    * Observações para os próximos passos, para subsidiar o próximo profissional a alterar o respectivo objeto;
     - Nada

--- Implementação/complementary_hours/application/views/cadastro.php
+++ Implementação/complementary_hours/application/views/cadastro.php
@@ -1,11 +1,11 @@
 
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
     <meta charset="utf-8">
     <title>Novo Usuário</title>
     
-<link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <style>
       .bd-placeholder-img {
@@ -22,146 +22,95 @@
           font-size: 3.5rem;
         }
       }
+      
     </style>
+    
+    <link href="form-validation.css" rel="stylesheet">
 
-  <link href="form-validation.css" rel="stylesheet">
   </head>
   <body class="bg-white">
-    <img src="<?php echo base_url('assets/img/logo_ifnmg.jpg');?>" width=”20px” height=”40px”>
-  <br>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home</a> 
-      <a class="nav-item nav-link" href="#">Ajuda</a>
-      <a class="nav-item nav-link" href="#">Sobre</a>
-    </div>
-  </div>
-    <button type="button" class="col-lg-1 btn btn-outline-danger btn-sm float-right">Logar</button>
-    <p>
-      <div class="col-auto">
+    <div class="col-12">
+        <img src="<?php echo base_url('assets/img/top-complementary_hours.png');?>" class="rounded mx-auto d-block"/>
+        <br>
+        <div class="accordion" id="accordionExample">
+
+          <div class="card-header rounded mx-auto col-sm-7" id="headingOne">
+              <form class="form-group" novalidate>
+                <div class="row">
+                  <div class="col-md-6 mb-3">
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                  </div>
+                    
+                  <div class="col-md-6 mb-3">
+                    <label for="sobre_nome">Sobrenome</label>
+                    <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-6 mb-3">
+                    <label for="data_de_nascimento">Data de Nascimento</label>
+                    <input type="date" class="form-control" id="firstName" placeholder="" value="" required>
+                  </div>
+           
+                  <div class="col-md-6 mb-3">
+                    <label for="telefone">Telefone</label>
+                    <input type="text" class="form-control" id="telefone" placeholder="" value="" pattern="\([0-9]{2}\)[0-9]{4,6}-[0-9]{3,4}$" required>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-12 mb-2">
+                    <label for="campus">Campus</label>
+                    <select class="custom-select">
+                      <option selected>Selecione</option>
+                      <option value="1">Instituto Federal do Norte de Minas Gerais Campus Arinos</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-12 mb-2">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="@aluno.ifnmg.edu.br">
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-6 mb-3">
+                    <label for="senha">Senha</label>
+                    <input type="password" class="form-control" id="senha" placeholder="" value="" required>
+                  </div>
+                    
+                  <div class="col-md-6 mb-3">
+                    <label for="repetir_senha">Repetir Senha</label>
+                    <input type="password" class="form-control" id="lastName" placeholder="" value="" required>
+                  </div>
+                </div>
+
+
+
+
+                <hr class="mb-4">
+                <div class="row">
+                  <div class="col-6 mb-5">
+                    <button class="col-12 btn btn-primary btn-lg" type="submit">Salvar</button>
+                  </div>
+                  <div class="col-6 mb-5">
+                    <a class="col-12 btn btn-danger btn-lg" href="<?php echo base_url('login')?>" role="button">Cancelar</a>
+                  </div>  
+                </div>
+              </form>
+          </div>
+          </div>
+
+        <footer class="my-5 pt-5 text-muted text-center text-small">
+            <p class="mb-1">&copy; 2020 Code System</p>
+        </footer>
       </div>
-    </p>
-    <button type="button" class="col-lg-1 btn btn-outline-success btn-sm float-right">Cadastrar</button>
-    
-  </nav>
-  <br>
-  <hr>
-  <br>
-      <div class="float-right col-lg-5">
-        <img src="<?php echo base_url('assets/img/logo.png');?>" width=”20%” height=”40%”>
-      </div>
-    <div class="col-md-8 order-md-1">
-
-
-
-    <div class="accordion" id="accordionExample">
-      <div class="card bg-white">
-        <div class="card-header" id="headingOne">
-
-      <form class="needs-validation form-group" novalidate>
-        <div class="row">
-          <div class="col-md-5 mb-3">
-            <label for="nome">Nome</label>
-            <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
-            <div class="invalid-feedback">
-              Valid first name is required.
-            </div>
-          </div>
-          <div class="col-md-5 mb-3">
-            <label for="sobre_nome">Sobre nome</label>
-            <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-            <div class="invalid-feedback">
-              Valid last name is required.
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-5 mb-3">
-            <label for="data_de_nascimento">Data de Nascimento</label>
-            <input type="date" class="form-control" id="firstName" placeholder="" value="" required>
-            <div class="invalid-feedback">
-              Valid first name is required.
-            </div>
-          </div>
-          <div class="col-md-5 mb-3">
-            <label for="telefone">Telefone</label>
-            <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-            <div class="invalid-feedback">
-              Valid last name is required.
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-10 mb-2">
-            <label for="campus">Campus</label>
-            <select class="custom-select">
-              <option selected>Selecione</option>
-              <option value="1">Intituto Federal do Norte de Minas Gerais Campus Arinos</option>
-              <option value="2">Intituto Federal do Norte de Minas Gerais Campus Januária</option>
-            </select>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-10 mb-2">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" placeholder="@aluno.ifnmg.edu.br">
-            <div class="invalid-feedback">
-              Please enter a valid email address for shipping updates.
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-5 mb-3">
-            <label for="senha">Senha</label>
-            <input type="password" class="form-control" id="senha" placeholder="" value="" required>
-            <div class="invalid-feedback">
-              Valid first name is required.
-            </div>
-          </div>
-          <div class="col-md-5 mb-3">
-            <label for="repetir_senha">Repetir Senha</label>
-            <input type="password" class="form-control" id="lastName" placeholder="" value="" required>
-            <div class="invalid-feedback">
-              Valid last name is required.
-            </div>
-          </div>
-        </div>
-    
-
-
-        
-        <hr class="mb-4">
-        <div class="row">
-          <div class="col-md-6 mb-3">
-            <button class="btn btn-success btn-lg" type="submit">Salvar</button>
-            <button class="btn btn-danger btn-lg" type="submit">Cancelar</button>
-          </div>  
-        </div>
-      </form>
-      </div>
-      </div>
-      </div>
-    </div>
-  </div>
-
-  <footer class="my-5 pt-5 text-muted text-center text-small">
-    <p class="mb-1">&copy; 2020 Code System</p>
-    <ul class="list-inline">
-      <li class="list-inline-item"><a href="#">Privacy</a></li>
-      <li class="list-inline-item"><a href="#">Terms</a></li>
-      <li class="list-inline-item"><a href="#">Support</a></li>
-    </ul>
-  </footer>
-</div>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
-        <script src="form-validation.js"></script></body>
+        <script src="form-validation.js"></script>
+    </body>
 </html>
--- Implementação/complementary_hours/application/views/login.php
+++ Implementação/complementary_hours/application/views/login.php
@@ -1,46 +1,76 @@
-<html lang="pt-br">
+
+<!doctype html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Página de produtos, usando Bootstrap.</title>
+    <title>Login</title>
 
-    <!-- Principal CSS do Bootstrap -->
-    <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet">
+<link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <!-- Estilos customizados para esse template -->
-    <link href="product.css" rel="stylesheet">
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    
+    <link href="floating-labels.css" rel="stylesheet">
   </head>
-
   <body>
+  <div class="col-12">
+  <br>
+  <br>
+  <div class="accordion" id="accordionExample">
 
-    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-      <div class="col-md-5 p-lg-5 mx-auto my-5">
-        <h1 class="display-4 font-weight-normal">Título legal</h1>
-        <p class="lead font-weight-normal">E uma descrição engraçadinha, para continuar. Alavanque seus esforços em marketing com esse exemplo, baseado nas páginas de marketing da Apple.</p>
-        <a class="btn btn-outline-secondary" href="#">Em breve</a>
+    <div class="card-header rounded mx-auto col-4" id="headingOne">
+    <img src="<?php echo base_url('assets/img/logo_complementary_hours.png');?>" class="rounded mx-auto d-block"/>
+  <form>
+      <div class="row">
+        <div class="col-md-12 mb-2">
+          <label for="email">Email</label>
+          <input type="email" class="form-control" id="email" placeholder="email@example.com">
+        </div>
       </div>
-      <div class="product-device shadow-sm d-none d-md-block"></div>
-      <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
-    </div> 
-
-    <!-- Principal JavaScript do Bootstrap
-    ================================================== -->
-    <!-- Foi colocado no final para a página carregar mais rápido -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/vendor/holder.min.js"></script>
-    <script>
-      Holder.addTheme('thumb', {
-        bg: '#55595c',
-        fg: '#eceeef',
-        text: 'Thumbnail'
-      });
-    </script>
-  </body>
+      <div class="row">
+        <div class="col-md-12 mb-2">
+          <label for="senha">Senha</label>
+          <input type="password" class="form-control" id="senha" placeholder="Password">
+        </div>
+      </div>
+      <br>
+      <div class="form-group">
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="dropdownCheck">
+          <label class="form-check-label" for="dropdownCheck">
+            Remember me
+          </label>
+          <a class="float-right text-dark" href="#">Esqueceu a senha?</a>
+        </div>
+      </div>
+      <br>
+      <button type="submit" class="rounded mx-auto d-block btn btn-primary col-6">Login</button>
+  </form>
+  <br>
+  <a class="text-center rounded mx-auto d-block col-12 text-dark" href="<?php echo base_url('cadastro')?>">Novo por aqui? Cadastre-se!</a>
+  </div>
+  
+  </div>
+  
+  </div>
+  
+  <footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">&copy; 2020 Code System</p>
+  </footer>
+</body>
 </html>
