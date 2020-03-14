18a1a5976693645cb84f16faa97af366e4f4f2e9
xXLuizXx <luizfernandofonseca123@gmail.com>
2020-03-14 19:24:01 -0300

    Atualização  do banco de dados e alteração na tela de cadastro

--- Implementação/banco_de_dados/complementary_hours.sql
+++ Implementação/banco_de_dados/complementary_hours.sql
@@ -1,176 +1,89 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6deb5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: 13-Mar-2020 às 22:26
--- Versão do servidor: 5.7.29-0ubuntu0.18.04.1
--- PHP Version: 7.2.24-0ubuntu0.18.04.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           5.7.29-0ubuntu0.18.04.1 - (Ubuntu)
+-- OS do Servidor:               Linux
+-- HeidiSQL Versão:              10.3.0.5771
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
---
--- Database: `complementary_hours`
---
 
--- --------------------------------------------------------
+-- Copiando estrutura do banco de dados para complementary_hours
+DROP DATABASE IF EXISTS `complementary_hours`;
+CREATE DATABASE IF NOT EXISTS `complementary_hours` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `complementary_hours`;
 
---
--- Estrutura da tabela `aluno`
---
-
-CREATE TABLE `aluno` (
-  `aluno_id` int(11) NOT NULL,
+-- Copiando estrutura para tabela complementary_hours.aluno
+DROP TABLE IF EXISTS `aluno`;
+CREATE TABLE IF NOT EXISTS `aluno` (
+  `aluno_id` int(11) NOT NULL AUTO_INCREMENT,
   `pessoa_id` int(11) NOT NULL,
-  `aluno_ano_inicio_curso` int(4) NOT NULL
+  `aluno_ano_inicio_curso` int(4) NOT NULL,
+  PRIMARY KEY (`aluno_id`),
+  KEY `FK_aluno_pessoa` (`pessoa_id`),
+  CONSTRAINT `FK_aluno_pessoa` FOREIGN KEY (`pessoa_id`) REFERENCES `pessoa` (`pessoa_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Copiando dados para a tabela complementary_hours.aluno: ~0 rows (aproximadamente)
+DELETE FROM `aluno`;
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
 
---
--- Estrutura da tabela `pessoa`
---
-
-CREATE TABLE `pessoa` (
-  `pessoa_id` int(11) NOT NULL,
+-- Copiando estrutura para tabela complementary_hours.pessoa
+DROP TABLE IF EXISTS `pessoa`;
+CREATE TABLE IF NOT EXISTS `pessoa` (
+  `pessoa_id` int(11) NOT NULL AUTO_INCREMENT,
   `usuario_id` int(11) NOT NULL,
   `pessoa_nome` varchar(100) NOT NULL,
   `pessoa_sobrenome` varchar(100) NOT NULL,
   `pessoa_data_nascimento` date NOT NULL,
   `pessoa_telefone` char(16) DEFAULT NULL,
   `pessoa_curso` char(100) NOT NULL,
-  `pessoa_campus` varchar(250) NOT NULL
+  `pessoa_campus` char(250) NOT NULL,
+  PRIMARY KEY (`pessoa_id`),
+  KEY `FK_pessoa_usuario` (`usuario_id`),
+  CONSTRAINT `FK_pessoa_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `pessoa`
---
+-- Copiando dados para a tabela complementary_hours.pessoa: ~0 rows (aproximadamente)
+DELETE FROM `pessoa`;
+/*!40000 ALTER TABLE `pessoa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pessoa` ENABLE KEYS */;
 
-INSERT INTO `pessoa` (`pessoa_id`, `usuario_id`, `pessoa_nome`, `pessoa_sobrenome`, `pessoa_data_nascimento`, `pessoa_telefone`, `pessoa_curso`, `pessoa_campus`) VALUES
-(1, 4, 'teste1', 'somente teste', '2000-03-01', '', '', ''),
-(2, 5, 'teste', 'so teste', '2000-03-01', '38998566623', '', ''),
-(3, 7, 'João Vitor', 'Farias Quintal', '2000-03-01', '351651811', '', '1');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `professor`
---
-
-CREATE TABLE `professor` (
-  `professor_id` int(11) NOT NULL,
-  `pessoa_id` int(11) NOT NULL
+-- Copiando estrutura para tabela complementary_hours.professor
+DROP TABLE IF EXISTS `professor`;
+CREATE TABLE IF NOT EXISTS `professor` (
+  `professor_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pessoa_id` int(11) NOT NULL,
+  PRIMARY KEY (`professor_id`),
+  KEY `FK_professor_pessoa` (`pessoa_id`),
+  CONSTRAINT `FK_professor_pessoa` FOREIGN KEY (`pessoa_id`) REFERENCES `pessoa` (`pessoa_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Copiando dados para a tabela complementary_hours.professor: ~0 rows (aproximadamente)
+DELETE FROM `professor`;
+/*!40000 ALTER TABLE `professor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `professor` ENABLE KEYS */;
 
---
--- Estrutura da tabela `usuario`
---
-
-CREATE TABLE `usuario` (
-  `usuario_id` int(11) NOT NULL,
+-- Copiando estrutura para tabela complementary_hours.usuario
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `usuario_id` int(11) NOT NULL AUTO_INCREMENT,
   `usuario_email` varchar(100) NOT NULL,
   `usuario_senha` varchar(100) NOT NULL,
-  `usuario_nivel` char(1) NOT NULL DEFAULT '1'
+  `usuario_nivel` char(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`usuario_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `usuario`
---
+-- Copiando dados para a tabela complementary_hours.usuario: ~0 rows (aproximadamente)
+DELETE FROM `usuario`;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 
-INSERT INTO `usuario` (`usuario_id`, `usuario_email`, `usuario_senha`, `usuario_nivel`) VALUES
-(1, 'user1@email.com', 'user1', '1'),
-(2, 'user2@email.com', 'user2', '2'),
-(3, 'user3@email.com', 'user3', '3'),
-(4, '', '', '1'),
-(5, 'teste@teste.com', 'teste', '1'),
-(7, 'testejoao@teste.com', 'joao', '1');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `aluno`
---
-ALTER TABLE `aluno`
-  ADD PRIMARY KEY (`aluno_id`),
-  ADD KEY `FK_aluno_pessoa` (`pessoa_id`);
-
---
--- Indexes for table `pessoa`
---
-ALTER TABLE `pessoa`
-  ADD PRIMARY KEY (`pessoa_id`),
-  ADD KEY `FK_pessoa_usuario` (`usuario_id`);
-
---
--- Indexes for table `professor`
---
-ALTER TABLE `professor`
-  ADD PRIMARY KEY (`professor_id`),
-  ADD KEY `FK_professor_pessoa` (`pessoa_id`);
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`usuario_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `aluno`
---
-ALTER TABLE `aluno`
-  MODIFY `aluno_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `pessoa`
---
-ALTER TABLE `pessoa`
-  MODIFY `pessoa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `professor`
---
-ALTER TABLE `professor`
-  MODIFY `professor_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- Constraints for dumped tables
---
-
---
--- Limitadores para a tabela `aluno`
---
-ALTER TABLE `aluno`
-  ADD CONSTRAINT `FK_aluno_pessoa` FOREIGN KEY (`pessoa_id`) REFERENCES `pessoa` (`pessoa_id`);
-
---
--- Limitadores para a tabela `pessoa`
---
-ALTER TABLE `pessoa`
-  ADD CONSTRAINT `FK_pessoa_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`);
-
---
--- Limitadores para a tabela `professor`
---
-ALTER TABLE `professor`
-  ADD CONSTRAINT `FK_professor_pessoa` FOREIGN KEY (`pessoa_id`) REFERENCES `pessoa` (`pessoa_id`);
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
