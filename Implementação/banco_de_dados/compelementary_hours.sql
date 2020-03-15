1d97b79ee9fe354f5d34246c5eea2775e07232a2
jvfq2000 <jvfq2000@gmail.com>
2020-03-14 21:58:41 -0300

    Banco de Dados

--- Implementação/banco_de_dados/compelementary_hours.sql
+++ Implementação/banco_de_dados/compelementary_hours.sql
@@ -33,6 +33,43 @@ DELETE FROM `aluno`;
 /*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
 /*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
 
+-- Copiando estrutura para tabela complementary_hours.campus
+DROP TABLE IF EXISTS `campus`;
+CREATE TABLE IF NOT EXISTS `campus` (
+  `campus_id` int(11) NOT NULL AUTO_INCREMENT,
+  `campus_descricao` varchar(100) NOT NULL,
+  `campus_cidade` varchar(100) NOT NULL,
+  `campus_estado` varchar(100) NOT NULL,
+  PRIMARY KEY (`campus_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela complementary_hours.campus: ~1 rows (aproximadamente)
+DELETE FROM `campus`;
+/*!40000 ALTER TABLE `campus` DISABLE KEYS */;
+INSERT INTO `campus` (`campus_id`, `campus_descricao`, `campus_cidade`, `campus_estado`) VALUES
+	(1, 'IFNMG - Campus Arinos', 'Arinos', 'Minas Gerais');
+/*!40000 ALTER TABLE `campus` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela complementary_hours.curso
+DROP TABLE IF EXISTS `curso`;
+CREATE TABLE IF NOT EXISTS `curso` (
+  `curso_id` int(11) NOT NULL AUTO_INCREMENT,
+  `curso_descricao` varchar(100) NOT NULL,
+  `curso_qtd_periodos` tinyint(4) NOT NULL,
+  PRIMARY KEY (`curso_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela complementary_hours.curso: ~5 rows (aproximadamente)
+DELETE FROM `curso`;
+/*!40000 ALTER TABLE `curso` DISABLE KEYS */;
+INSERT INTO `curso` (`curso_id`, `curso_descricao`, `curso_qtd_periodos`) VALUES
+	(1, 'Bacharelado em Administração', 8),
+	(2, 'Bacharelado em Agronomia', 10),
+	(3, 'Bacharelado em Sistemas de Informação', 8),
+	(4, 'Tecnologia em Gestão Ambiental', 6),
+	(5, 'Tecnologia em Produção de Grãos', 6);
+/*!40000 ALTER TABLE `curso` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela complementary_hours.pessoa
 DROP TABLE IF EXISTS `pessoa`;
 CREATE TABLE IF NOT EXISTS `pessoa` (
@@ -42,11 +79,15 @@ CREATE TABLE IF NOT EXISTS `pessoa` (
   `pessoa_sobrenome` varchar(100) NOT NULL,
   `pessoa_data_nascimento` date NOT NULL,
   `pessoa_telefone` char(16) DEFAULT NULL,
-  `pessoa_curso` char(100) NOT NULL,
-  `pessoa_campus` char(250) NOT NULL,
+  `curso_id` int(11) NOT NULL,
+  `campus_id` int(11) NOT NULL,
   PRIMARY KEY (`pessoa_id`),
   KEY `FK_pessoa_usuario` (`usuario_id`),
-  CONSTRAINT `FK_pessoa_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`)
+  KEY `fk_pessoa_curso` (`curso_id`),
+  KEY `fk_pessoa_campus` (`campus_id`),
+  CONSTRAINT `FK_pessoa_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`),
+  CONSTRAINT `fk_pessoa_campus` FOREIGN KEY (`campus_id`) REFERENCES `campus` (`campus_id`),
+  CONSTRAINT `fk_pessoa_curso` FOREIGN KEY (`curso_id`) REFERENCES `curso` (`curso_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Copiando dados para a tabela complementary_hours.pessoa: ~0 rows (aproximadamente)
@@ -75,13 +116,15 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `usuario_id` int(11) NOT NULL AUTO_INCREMENT,
   `usuario_email` varchar(100) NOT NULL,
   `usuario_senha` varchar(100) NOT NULL,
-  `usuario_nivel` char(1) NOT NULL DEFAULT '1',
+  `usuario_nivel` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`usuario_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Copiando dados para a tabela complementary_hours.usuario: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela complementary_hours.usuario: ~1 rows (aproximadamente)
 DELETE FROM `usuario`;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` (`usuario_id`, `usuario_email`, `usuario_senha`, `usuario_nivel`) VALUES
+	(1, 'jvfq@aluno.ifnmg.edu.br', '123', 1);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
