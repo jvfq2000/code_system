bf4f622ea342f5702b84ab1464293d58e35698ca
jvfq2000 <jvfq2000@gmail.com>
2020-03-19 21:33:27 -0300

    Mudança Drastica

--- complementary_hours/application/config/config.php
+++ complementary_hours/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/complementary_hours/';
+$config['base_url'] = 'http://localhost/code_system/complementary_hours/';
 
 /*
 |--------------------------------------------------------------------------
