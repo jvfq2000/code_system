e8b328cf64c3102f84d63df916160ee6e644bdb4
xXLuizXx <luizfernandofonseca123@gmail.com>
2020-10-19 15:29:39 -0300

    Criacao do gerenciamento de regulamentos

---
+++ complementary_hours/application/controllers/Regulamento.php
@@ -0,0 +1,205 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Regulamento extends CI_Controller {
+	private $dados;
+
+	function __construct(){
+		parent::__construct();
+
+		$this->load->model('classes/Regulamento_model', 'regulamento');
+		$this->load->model('classes/Campus_model', 'campus');
+		$this->load->model('classes/Curso_model', 'curso');
+
+		$this->dados['campus_options'] = $this->campus->montar_options_campus();
+		$this->dados['linhas']         = $this->regulamento->montar_tabela_regulamento();
+        $this->dados['pasta']          = 'assets/Regulamentos/';
+		$this->dados['tentou']         = FALSE;
+		$this->dados['tentou_salvar']  = FALSE;
+        $this->dados['pegou_regulamento']  = 'N';
+		$this->dados['sucesso']        = FALSE;
+		$this->dados['excluiu']        = FALSE;
+		$this->dados['mensagem']       = "";
+	}
+	
+	public function index(){
+		$this->dados['mostrar']       = "tabela";
+        $this->dados['sucesso']       = FALSE;
+        $header['titulo']             = 'Gerenciar Regulamentos';
+        
+        $this->load->view('include/header', $header);
+		$this->load->view('include/menu');
+		$this->load->view('gerenciar_regulamento', $this->dados);
+		$this->load->view('include/footer');
+	}
+
+	public function ajax_mostrar_cursos(){
+		$campus_id = $this->input->post('id');
+		echo $this->curso->montar_options_curso($campus_id);
+	}
+
+    public function novo(){
+        $this->dados['mostrar']      = "operacoes";
+        $this->dados['tentou']         = FALSE;
+		$this->dados['sucesso']        = FALSE;
+        $header['titulo']            = 'Gerenciar Regulamentos';
+        
+        $this->load->view('include/header', $header);
+		$this->load->view('include/menu');
+		$this->load->view('gerenciar_regulamento', $this->dados);
+		$this->load->view('include/footer');
+	}
+    
+	public function cadastrar(){
+		$this->dados['mensagem']      = "Erro ao cadastrar, tente novamente!";
+        
+        if(isset($_POST['enviar'])){
+            $formatos = array("pdf");
+            $arquivos = $_FILES['arquivos'];
+            $nomes_arquivos = $arquivos['name'];
+            
+            for($i = 0; $i < count($nomes_arquivos); $i++){
+
+                $this->regulamento->setCampus_id($this->input->post("campus"));
+                $this->regulamento->setCurso_id($this->input->post("curso"));
+                $this->regulamento->setRegulamento_descricao($this->input->post("regulamento_descricao"));
+                $this->regulamento->setRegulamento_ano($this->input->post("regulamento_ano"));
+                
+                
+                $extensao = explode('.', $nomes_arquivos[$i]);
+                $extensao = end($extensao);
+                $nomes_arquivos[$i] = uniqid().".$extensao";
+                
+                if(in_array($extensao, $formatos)){
+                    $move = move_uploaded_file($_FILES['arquivos']['tmp_name'][$i], $this->dados['pasta'].$nomes_arquivos[$i]);
+                    $this->regulamento->setRegulamento_caminho($nomes_arquivos[$i]);
+                    $this->regulamento->cadastrar();
+                    $this->dados['sucesso']        = TRUE;
+                    $this->dados['mensagem'] = "Regulamento salvo com sucesso";
+                } else{
+                    $this->dados['tentou_salvar']        = TRUE;
+                    $this->dados['mensagem'] = "Erro ao enviar arquivo, certifique-se de que o formato do seu arquivo seja PDF";
+                }
+            }
+        }
+        
+        $this->dados['mostrar'] = "operacoes";
+        $header['titulo']       = 'Gerenciar Regulamentos';
+        
+        $this->load->view('include/header', $header);
+		$this->load->view('include/menu');
+		$this->load->view('gerenciar_regulamento', $this->dados);
+		$this->load->view('include/footer');
+    }
+    
+    public function editar($regulamento_id){
+        $this->dados['mostrar']       = "operacoes";
+        $header['titulo']             = 'Gerenciar Regulamentos';
+        $this->dados['pegou_regulamento']  = 'S';
+        
+        $this->regulamento->setRegulamento_id($regulamento_id);
+        $result = $this->regulamento->pegar_regulamento();
+        $regulamento_result = $result->row_array();
+        
+        $this->dados['regulamento_id']        = $regulamento_result['regulamento_id'];
+        $this->dados['regulamento_descricao'] = $regulamento_result['regulamento_descricao'];
+        $this->dados['regulamento_ano']       = $regulamento_result['regulamento_ano'];
+        
+        $this->load->view('include/header', $header);
+		$this->load->view('include/menu');
+		$this->load->view('gerenciar_regulamento', $this->dados);
+		$this->load->view('include/footer');
+	}
+    
+    public function salvar_edicao($regulamento_id){
+        $this->dados['tentou']   = TRUE;
+		$this->dados['mensagem'] = "Erro ao salvar, tente novamente!";
+        
+        $this->regulamento->setRegulamento_id($regulamento_id);
+        $this->regulamento->setCampus_id($this->input->post("campus"));
+        $this->regulamento->setCurso_id($this->input->post("curso"));
+        $this->regulamento->setRegulamento_descricao($this->input->post("regulamento_descricao"));
+        $this->regulamento->setRegulamento_ano($this->input->post("regulamento_ano"));
+  
+        if(isset($_POST['enviar'])){
+            $formatos = array("pdf");
+            $arquivos = $_FILES['arquivos'];
+            $nomes_arquivos = $arquivos['name'];
+            
+            for($i = 0; $i < count($nomes_arquivos); $i++){
+                $extensao = explode('.', $nomes_arquivos[$i]);
+                $extensao = end($extensao);
+                $nomes_arquivos[$i] = uniqid().".$extensao";
+                
+                if(in_array($extensao, $formatos)){
+                    $move = move_uploaded_file($_FILES['arquivos']['tmp_name'][$i], $this->dados['pasta'].$nomes_arquivos[$i]);
+                    $this->regulamento->setRegulamento_caminho($nomes_arquivos[$i]);
+                    $this->regulamento->alterar();
+                    $this->dados['sucesso']        = TRUE;
+                    $this->dados['mensagem'] = "Regulamento alterado com sucesso";
+                } else{
+                    $this->dados['tentou_salvar']        = TRUE;
+                    $this->dados['mensagem'] = "Erro ao enviar arquivo, certifique-se de que o formato do seu arquivo seja PDF";
+                }
+            }
+        }
+        
+        $this->dados['mostrar'] = "operacoes";
+        $header['titulo']       = 'Gerenciar Regulamentos';
+        
+        $this->load->view('include/header', $header);
+		$this->load->view('include/menu');
+		$this->load->view('gerenciar_regulamento', $this->dados);
+		$this->load->view('include/footer');
+    }
+    
+    public function excluir($regulamento_id){
+        $this->dados['mostrar']  = "tabela";
+        $header['titulo']        = 'Gerenciar Regulamentos';
+        $this->dados['mensagem'] = "Erro ao cadastrar, tente novamente!";
+        $this->dados['sucesso']  = FALSE;
+        
+        
+        $this->regulamento->setRegulamento_id($regulamento_id);
+        $regulamento_excluido = $this->regulamento->excluir();
+        
+        if ($regulamento_excluido) {
+            $this->dados['sucesso']  = TRUE;
+            $this->dados['tentou']   = TRUE;
+            $this->dados['excluiu']  = TRUE;
+            $this->dados['mensagem'] = "Campus excluido com sucesso!";
+        }
+        
+        $this->load->view('include/header', $header);
+		$this->load->view('include/menu');
+		$this->load->view('gerenciar_regulamento', $this->dados);
+		$this->load->view('include/footer');
+	}
+    public function confirmacao($regulamento_id){
+        
+        $this->dados['link_confirmou'] = base_url('Regulamento/excluir/').$regulamento_id;
+        $this->dados['link_cancelou']  = base_url('Regulamento');
+        $this->dados['titulo']         = 'Confirmação';
+        
+        $this->dados['linhas']         = $this->regulamento->montar_tabela_regulamento();
+        $this->load->view('include/header', $this->dados);
+		$this->load->view('include/modal_excluir', $this->dados);
+        $this->load->view('include/footer');
+	}
+    
+    public function pesquisar(){
+        $this->regulamento->setPesquisa($this->input->post("pesquisar"));
+        $this->regulamento->pesquisar();
+        
+        $this->dados['mostrar']       = "tabela";
+        $this->dados['sucesso']       = FALSE;
+        $this->dados['linhas']        = $this->regulamento->montar_tabela_pesquisa();
+        $header['titulo']             = 'Gerenciar Regulamento';
+        
+        $this->load->view('include/header', $header);
+		$this->load->view('include/menu');
+		$this->load->view('gerenciar_regulamento', $this->dados);
+		$this->load->view('include/footer');
+	}
+    
+}
---
+++ complementary_hours/application/models/classes/Regulamento_model.php
@@ -0,0 +1,188 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Regulamento_model extends CI_Model {
+	private $regulamento_id;
+	private $campus_id;
+	private $curso_id;
+	private $regulamento_descricao;
+	private $regulamento_caminho;
+	private $regulamento_ano;
+	private $pesquisa;
+
+	function __construct(){
+		parent::__construct();
+	}
+	
+	public function getRegulamento_id(){
+		return $this->regulamento_id;
+	}
+	public function setRegulamento_id($regulamento_id){
+		$this->regulamento_id = $regulamento_id;
+	}
+
+	public function getCampus_id(){
+		return $this->campus_id;
+	}
+	public function setCampus_id($campus_id){
+		$this->campus_id = $campus_id;
+	}
+
+	public function getCurso_id(){
+		return $this->curso_id;
+	}
+	public function setCurso_id($curso_id){
+		$this->curso_id = $curso_id;
+	}
+
+	public function getRegulamento_descricao(){
+		return $this->regulamento_descricao;
+	}
+	public function setRegulamento_descricao($regulamento_descricao){
+		$this->regulamento_descricao = $regulamento_descricao;
+	}
+    
+    public function getRegulamento_caminho(){
+		return $this->regulamento_caminho;
+	}
+	public function setRegulamento_caminho($regulamento_caminho){
+		$this->regulamento_caminho = $regulamento_caminho;
+	}
+    
+    public function getRegulamento_ano(){
+		return $this->regulamento_ano;
+	}
+	public function setRegulamento_ano($regulamento_ano){
+		$this->regulamento_ano = $regulamento_ano;
+	}
+    
+    public function getPesquisa(){
+		return $this->pesquisa;
+	}
+	public function setPesquisa($pesquisa){
+		$this->pesquisa = $pesquisa;
+	}
+    
+	public function cadastrar(){
+		$dados_regulamento = array(
+			"regulamento_id"         => $this->getRegulamento_id(),
+			"campus_id"              => $this->getCampus_id(),
+			"curso_id"               => $this->getCurso_id(),
+            "regulamento_descricao"  => $this->getRegulamento_descricao(),
+            "regulamento_caminho"    => $this->getRegulamento_caminho(),
+            "regulamento_ano"        => $this->getRegulamento_ano()
+		);
+        return $this->db->insert("regulamento", $dados_regulamento);
+	}
+    
+    
+    public function listar_regulamentos($regulamento_id){
+		$this->db->select('*');
+		$this->db->from('regulamento');
+		$this->db->where('regulamento_id', $regulamento_id);
+		$this->db->order_by('regulamento_descricao');
+		$query = $this->db->get();
+		return $query;
+	}
+    
+    public function listar_regulamentos_tabela(){
+		$this->db->select('regulamento_id, curso_descricao, campus_descricao, regulamento_descricao, regulamento_ano');
+		$this->db->from('regulamento');
+        $this->db->join('campus', 'campus.campus_id = regulamento.campus_id');
+        $this->db->join('curso', 'curso.curso_id = regulamento.curso_id');
+		$this->db->order_by('regulamento_descricao');
+		$query = $this->db->get();
+		return $query;
+	}	
+    
+    public function editar_curso(){
+        $dados = array(
+                'campus_id' => $this->getCampus_id(),
+                'curso_descricao' => $this->getCurso_descricao(),
+                'curso_qtd_periodos' => $this->getCurso_qtd_periodos()
+            );
+        $where = "campus_id = {$this->getCampus_id()}";
+        $query = $this->db->update_string('curso', $dados, $where);
+        return $this->db->query($query);
+    }
+
+    public function montar_tabela_regulamento(){
+		$regulamento_lista = $this->listar_regulamentos_tabela();
+        $linhas = "";
+
+		foreach($regulamento_lista->result() as $regulamento){
+            $linhas .= "<tr>";
+            $linhas .= "<td>{$regulamento->campus_descricao}</td>";
+			$linhas .= "<td>{$regulamento->curso_descricao}</td>";
+            $linhas .= "<td>{$regulamento->regulamento_descricao}</td>";
+            $linhas .= "<td>{$regulamento->regulamento_ano}</td>";
+            $linhas .= "<td><div class=\"row\"><a href=\"".base_url('Regulamento/editar/')."{$regulamento->regulamento_id}\" />";
+            $linhas .= "<img src=\"".base_url('assets/img/icone/editar.png')."\" class=\"nav-link\" width=\"55\" height=\"40\" /></a>";
+            $linhas .= "<a href=\"".base_url('Regulamento/confirmacao/')."{$regulamento->regulamento_id}\"/>";
+            $linhas .= "<img src=\"".base_url('assets/img/icone/lixeira.png')."\" class=\"nav-link\" width=\"55\" height=\"39\" /></a></td>";
+            $linhas .= "</div><tr>";        
+		}
+        
+		return $linhas;
+	}
+    
+    public function pegar_regulamento(){
+		$this->db->select('regulamento_id, curso_descricao, campus_descricao, regulamento_descricao, regulamento_ano');
+		$this->db->from('regulamento');
+        $this->db->join('campus', 'campus.campus_id = regulamento.campus_id');
+        $this->db->join('curso', 'curso.curso_id = regulamento.curso_id');
+        $this->db->where('regulamento_id', $this->getRegulamento_id());
+		$query = $this->db->get();
+		return $query;
+	}
+    
+    public function pesquisar(){
+		$this->db->select('*');
+		$this->db->from('regulamento');
+        $this->db->join('campus', 'campus.campus_id = regulamento.campus_id');
+        $this->db->join('curso', 'curso.curso_id = regulamento.curso_id');
+        $this->db->like('regulamento_descricao',$this->getPesquisa());
+		$this->db->order_by('regulamento_descricao');
+		$query = $this->db->get();
+		return $query;
+	}
+    
+     public function montar_tabela_pesquisa(){
+		$regulamentos_lista = $this->pesquisar();
+        $linhas = "";
+
+		foreach($regulamentos_lista->result() as $regulamento){
+            $linhas .= "<tr>";
+            $linhas .= "<td>{$regulamento->campus_descricao}</td>";
+			$linhas .= "<td>{$regulamento->curso_descricao}</td>";
+            $linhas .= "<td>{$regulamento->regulamento_descricao}</td>";
+            $linhas .= "<td>{$regulamento->regulamento_ano}</td>";
+            $linhas .= "<td><div class=\"row\"><a href=\"".base_url('Regulamento/editar/')."{$regulamento->regulamento_id}\" />";
+            $linhas .= "<img src=\"".base_url('assets/img/icone/editar.png')."\" class=\"nav-link\" width=\"55\" height=\"40\" /></a>";
+            $linhas .= "<a href=\"".base_url('Regulamento/confirmacao/')."{$regulamento->regulamento_id}\"/>";
+            $linhas .= "<img src=\"".base_url('assets/img/icone/lixeira.png')."\" class=\"nav-link\" width=\"55\" height=\"39\" /></a></td>";
+            $linhas .= "</div><tr>";          
+		}
+        
+		return $linhas;
+	}
+  
+    public function alterar(){
+        $dados = array(
+                        "regulamento_id"         => $this->getRegulamento_id(),
+                        "campus_id"              => $this->getCampus_id(),
+                        "curso_id"               => $this->getCurso_id(),
+                        "regulamento_descricao"  => $this->getRegulamento_descricao(),
+                        "regulamento_caminho"    => $this->getRegulamento_caminho(),
+                        "regulamento_ano"        => $this->getRegulamento_ano()
+                    );
+    	$where = "regulamento_id = {$this->getRegulamento_id()}";
+        $query = $this->db->update_string('regulamento', $dados, $where);
+        return $this->db->query($query);
+    }
+    
+    public function excluir(){
+        return $this->db->delete('regulamento', array('regulamento_id' => $this->getRegulamento_id())); 
+    }
+    
+}
